b35b6e4ccedb426d0a1626becdd97bea0365ee82
DanXxxxx <danxu0910@gmail.com>
2025-08-10 11:25:08 +0200

    first prototype

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-// window.addEventListener("click", openNew);
-
-// function openNew() {
-//     console.log("click")
-//     window.open(window.location.href, "mozillaWindow", "popup=true")
-// }
-
-// set background color 
-// document.body.style.backgroundColor = "Aqua";
-
 window.addEventListener("resize", changeSize);
 
+let height = screen.height;
+let width = screen.width;
 let w, h, x, y;
-let vol, osc;
+let vol, osc, panner;
+let min_fre = 40;
+let max_fre = 5000;
+let base = (max_fre - min_fre)**(1/height);
 
 document.addEventListener('click', initTone, { once: true });
 
@@ -19,33 +14,48 @@ async function initTone() {
   // Start Tone.js context
   await Tone.start();
   console.log('Audio is ready');
+  let ini_fre = base ** (height - window.screenTop);
+
+  osc = new Tone.Oscillator(ini_fre + min_fre).start();
   
   // Now you can schedule/play sounds
-  vol = new Tone.Volume(-12).toDestination();
-  osc = new Tone.Oscillator().connect(vol).start();
-
+  vol = new Tone.Volume().toDestination();
+  changeSize();
+  panner = new Tone.Panner(1).toDestination();
   detectPosition();
+  osc.connect(panner).connect(vol);
 }
 
 function changeSize() {
     h = window.innerHeight;
     w = window.innerWidth;
-    console.log('size: ' + w + ', ' + h)
-    vol.volume.value = mapValue(w, 0, screen.width, -20, -1);
-    console.log(vol.volume.value);
+    // console.log('size: ' + w + ', ' + h);
+    let v = mapValue(h*w, 0, width*height, -40, -1);
+    vol.volume.rampTo(v, 0.05);
+    // console.log(vol.volume.value);
 }
 
 function detectPosition() {
-    if (!window.screenLeft) {
-        window.screenLeft = window.screenX;
-        window.screenTop = window.screenY;
-    }
+    // fuck things up
+    // if (!window.screenLeft) {
+    //     window.screenLeft = window.screenX;
+    //     window.screenTop = window.screenY;
+    // }
 
     x = window.screenLeft;
     y = window.screenTop;
 
-    osc.frequency.value = screen.height - y + 20;
-    // console.log(osc.frequency.value)
+    // let fre = mapValue(height-y, 0, height, min_fre, max_fre);
+    let fre = base ** (height - y);
+    console.log(fre);
+
+    if (fre > 0) {
+        osc.frequency.rampTo(fre + min_fre, 0.05);
+    }
+
+    let p = mapValue(x, 0, width, -1, 1);
+    console.log(p);
+    panner.pan.rampTo(p, 0.1);
 
     window.requestAnimationFrame(detectPosition);
 }
